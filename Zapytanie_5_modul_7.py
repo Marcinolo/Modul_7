e645c75cef5e0f0372de4321d628d4673ee1d477
Marcinolo <143748129+Marcinolo@users.noreply.github.com>
2024-02-25 18:09:34 +0100

    Add files via upload

---
+++ Zapytanie_5_modul_7.py
@@ -0,0 +1,30 @@
+# SELECT s.subject_name, s.lecturer_id, l.last_name from Subjects s
+# JOIN Lecturers l ON l.id = s.lecturer_id
+# GROUP BY s.subject_name
+
+from sqlalchemy import func
+from sql_alchemy import Grade, Student, Subject, Group, Base, Lecturer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import aliased
+
+engine = create_engine('sqlite:///example4.db', echo = True)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+l_lecturer = aliased(Lecturer)
+
+query_result = session.query(
+    Subject.subject_name,
+    Subject.lecturer_id,
+    l_lecturer.last_name
+).join(
+    l_lecturer,
+    Subject.lecturer_id == l_lecturer.id
+).group_by(
+    Subject.subject_name
+).all()
+
+for row in query_result:
+    print(row)
